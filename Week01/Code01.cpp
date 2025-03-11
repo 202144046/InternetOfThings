3d4e173537c51e14ef75843cafc90bfd08801731
minjae <minjae54920@gmail.com>
2025-03-11 14:48:06 +0900

    20250311_1448

--- Week01/Code01.cpp
+++ Week01/Code01.cpp
@@ -3,6 +3,7 @@
 void setup()
 {
   pinMode(7, OUTPUT);
+  pinMode(8, OUTPUT);
 }
 
 void loop()
@@ -10,6 +11,10 @@ void loop()
   digitalWrite(7, HIGH);
   delay(1000); // Wait for 1000 millisecond(s)
   digitalWrite(7, LOW);
+  delay(1000); // Wait for 1000 millisecond(s)
+  digitalWrite(8, HIGH);
+  delay(1000); // Wait for 1000 millisecond(s)
+  digitalWrite(8, LOW);
   delay(1000); // Wait for 1000 millisecond(s)
 }
 
