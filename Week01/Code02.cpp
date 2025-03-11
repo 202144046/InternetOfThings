403c78d4c45ee045c1f62771a78bea591d268cf4
minjae <minjae54920@gmail.com>
2025-03-11 15:54:23 +0900

    20250311_1554

--- Week01/Code02.cpp
+++ Week01/Code02.cpp
@@ -20,30 +20,6 @@ void setup()
 void loop()
 {
   
-  long duration, distance;
-  delayMicroseconds(2); //delay(2)
-  digitalWrite(TRIG, HIGH);// C++ code
-//
-
-#define TRIG 13 // TRIG ги
-#define ECHO 12 // ECHO ги
-
-int led_r = 7;
-int led_g = 8;
-
-
-void setup()
-{
-  Serial.begin(9600);
-  pinMode(led_r, OUTPUT);
-  pinMode(led_g, OUTPUT);
-  pinMode(TRIG, OUTPUT);
-  pinMode(ECHO, INPUT);
-}
-
-void loop()
-{
-  
   
   
   long duration, distance;
@@ -58,7 +34,7 @@ void loop()
   Serial.println(duration);
   Serial.print("\nDistance : ");
   Serial.print(distance);
-  Serial.println(" CM");
+  Serial.println(" cm");
   
   if (distance < 100)
   {digitalWrite (led_r, HIGH);
@@ -66,22 +42,6 @@ void loop()
   else
   {digitalWrite (led_r, LOW);
    digitalWrite (led_g, HIGH);}
-  
-  delay(1000); // Wait for 1000 millisecond(s)
-  
-}
-
-
-  delayMicroseconds(10);
-  digitalWrite(TRIG, LOW);
-  
-  duration = pulseIn(ECHO, HIGH);
-  
-  distance = duration / 58.2;
-  Serial.println(duration);
-  Serial.print("\nDistance : ");
-  Serial.print(distance);
-  Serial.println(" Cm");
   
   delay(1000); // Wait for 1000 millisecond(s)
   
