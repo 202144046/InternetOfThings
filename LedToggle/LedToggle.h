76e5d8b82a45082986343ad1ca2f27adfe42cd62
Minjae <128676814+202144046@users.noreply.github.com>
2025-04-15 15:15:48 +0900

    Add files via upload

---
+++ LedToggle/LedToggle.h
@@ -0,0 +1,19 @@
+#ifndef LedToggle_h
+#define LedToggle_h
+
+#include "Arduino.h"
+
+class LedToggle {
+    public:
+    LedToggle(int pin);     // 생성자
+    LedToggle(int pin, unsigned long delayTime)
+    void toggle();          // LED 상태 토글
+
+
+    private:
+    int _pin;
+    unsigned long _delayTime;
+    bool _state;
+};
+
+#endif
